963ba9e1abe73d4d87c6decd14f483b9ba51ef79
yashu879 <yashaswini.bakkesh@gmail.com>
2024-09-26 13:12:06 -0700

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 class DataIngestionConfig:
    train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -46,5 +48,8 @@ if __name__=="__main__":
    train_data,test_data=obj.initiate_data_ingestion()
    data_transformation=DataTransformation()
    train_arr,test_arr,_=data_transformation.initiate_data_trasformation(train_data,test_data)
+
+   modeltrainer=ModelTrainer()
+   print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
     
